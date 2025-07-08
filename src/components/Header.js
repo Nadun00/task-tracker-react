07e781fd03dfcbd001ad9f7859d32f36902fce14
Nadun00 <it22608840@my.sliit.lk>
2025-07-09 00:05:14 +0530

    add some style to header check

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 import React from "react"
 
-const Header = ({title}) => {
+const Header = ({title ='Task Tracker'}) => {
   return( 
-  <header>
+  <header style= {{ backgroundColor: 'lightblue', padding: '20px' }}>
       <h1>{title}</h1>
   </header>
-  )
+  );
 }
 
 Header.defaultProps = {
   title: 'Task Tracker',
 }
 
-export default Header
+export default Header;
