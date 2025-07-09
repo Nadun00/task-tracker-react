82cd0c31b34f2800ec0188ba6bcd87b50c3f1b66
Nadun00 <it22608840@my.sliit.lk>
2025-07-09 19:29:57 +0530

    change props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
-import React from "react"
+import PropTypes from 'prop-types'
 
-const Header = ({title ='Task Tracker'}) => {
+const Header = ({title}) => {
   return( 
   <header >
       <h1>{title}</h1>
@@ -10,6 +10,10 @@ const Header = ({title ='Task Tracker'}) => {
 
 Header.defaultProps = {
   title: 'Task Tracker',
+}
+
+Header.propTypes = {
+  title:PropTypes.string.isRequired,
 }
 
 export default Header;
